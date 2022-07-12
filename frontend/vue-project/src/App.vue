df471ec03595aba9228e6021f0c5c5899ac87d64
AndrewKorn <a.korneshchuk1@g.nsu.ru>
2022-07-12 16:16:45 +0700

    add all

---
+++ frontend/vue-project/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app">
+    <h1>Hello, there you can make your own playlist with your friends!</h1>
+    <hr>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "app",
+    components: {
+
+    }
+  }
+</script>
+
+
+<style>
+  .app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
