4f2ac385f22a71a0accfc8aadf1bc63b1fbbaf49
AndrewKorn <a.korneshchuk1@g.nsu.ru>
2022-07-14 14:03:51 +0700

    add sc widget and some work on front

--- frontend/vue-project/src/components/SongTable.vue
+++ frontend/vue-project/src/components/SongTable.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-
     <div v-for="song in songs">
       <iframe width="95%" height="100" :src='"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + song.song_id + ""'></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>
     </div>
