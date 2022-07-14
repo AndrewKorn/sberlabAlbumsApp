332d6e0107ec882143f35feef9febd18c0921e9a
AndrewKorn <a.korneshchuk1@g.nsu.ru>
2022-07-14 14:00:11 +0700

    add sc widget and some work on front

--- frontend/vue-project/src/App.vue
+++ frontend/vue-project/src/App.vue
@@ -3,7 +3,7 @@
     <div class="header">
       <div class="qwe">
         <h1 style="margin-top: 5px; margin-left: 10px; font-size: 60px">Be part'y</h1>
-        <img style="margin-left: 30px; margin-top: 15px;" src="jigsaw.png" width="80" height="80">
+        <img src="jigsaw.png" width="80" height="80">
       </div>
     </div>
     <h1 class="hello">Hello, there you can make your own playlist with your friends!</h1>
@@ -40,5 +40,9 @@
 
   .qwe {
     display: flex;
+  }
+
+  img {
+    margin-left: 30px; margin-top: 15px;
   }
 </style>
